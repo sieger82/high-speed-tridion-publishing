1d0c6751aae67f5c8b365cfc3834b320c3a19dc3
sieger82 <sieger@veenhoven.net>
2015-05-11 13:39:55 +0200

    added some explanatory comments

--- remote-publishing-server/start_transfer.bat
+++ remote-publishing-server/start_transfer.bat
@@ -11,11 +11,12 @@ dir /b /s "C:\ftpupload\incoming\*.state.xml" > C:\uftp_exe_W7-4.6.1\myFiles.txt
 
 REM send the xml files
 if NOT ERRORLEVEL 1 (
-    C:\uftp_exe_W7-4.6.1\uftp -U 0x11113333 -Y aes256-ccm -e ecdh_rsa -b 1024 -B 104857600 -z -R -1 -M 10.0.0.2 -i C:\uftp_exe_W7-4.6.1\myFiles.txt
+    REM option: -Y and -e encryption type. -b UDP datapacket size. -B send buffer size. -z sync mode. 
+    REM -R max transfer speed (-1 = as fast as possible). -M remote server address -i list of files to send
+    C:\uftp_exe_W7-4.6.1\uftp -Y aes256-ccm -e ecdh_rsa -b 1024 -B 104857600 -z -R -1 -M 10.0.0.2 -i C:\uftp_exe_W7-4.6.1\myFiles.txt
 ) else timeout 10
 
 REM delete xml files older than 15 minutes
 call C:\uftp_exe_W7-4.6.1\delete_old_files.vbs
-
 
 goto loop
