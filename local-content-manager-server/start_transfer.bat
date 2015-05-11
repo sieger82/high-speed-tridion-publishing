6caa4da31fc40d21b614e681e2d6a9eb373b9296
sieger82 <sieger@veenhoven.net>
2015-05-11 13:39:14 +0200

    added some explanatory comments

--- local-content-manager-server/start_transfer.bat
+++ local-content-manager-server/start_transfer.bat
@@ -11,7 +11,9 @@ dir /b /s "D:\ftpchina\incoming\*.zip" > D:\uftp_exe_W7-4.6.1\myFiles.txt
 
 REM send the zip files and delete when complete
 if NOT ERRORLEVEL 1 (
-    D:\uftp_exe_W7-4.6.1\uftp -U 0x11112222 -Y aes256-ccm -e ecdh_rsa -b 1024 -B 104857600 -z -R -1 -M 10.0.0.1 -i D:\uftp_exe_W7-4.6.1\myFiles.txt
+    REM option: -Y and -e encryption type. -b UDP datapacket size. -B send buffer size. -z sync mode. 
+    REM -R max transfer speed (-1 = as fast as possible). -M remote server address -i list of files to send
+    D:\uftp_exe_W7-4.6.1\uftp -Y aes256-ccm -e ecdh_rsa -b 1024 -B 104857600 -z -R -1 -M 10.0.0.1 -i D:\uftp_exe_W7-4.6.1\myFiles.txt
     if NOT ERRORLEVEL 1 (
         FOR /F "delims=" %%f IN (D:\uftp_exe_W7-4.6.1\myFiles.txt) DO (
             del "%%f" 
